072f38ea8ac6c7b7d5bdb2acada05965c096aaed
Iván <87248277+Ivan-Divinsky@users.noreply.github.com>
2024-09-24 12:53:06 -0300

    Editando el src, eliminando archivos y armando el app.js con sus rutas

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
 import './App.css';
-
 import { Switch, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 import Home from './screens/Home';
-import Detalle from './screens/detalle';
-import Favoritos from './screens/favoritos';
-import Busqueda from './screens/busqueda';
-import VerTodas from './screens/verTodas';
-
-import Loader from './screens/loader';
-import NotFound from './screens/notFound';
-
-
+import NotFound from './screens/NotFound';
+import Detalle from './screens/Detalle';
+import AboutUs from './screens/AboutUs';
+import Favoritos from './screens/Favoritos';
+import Generos from './screens/Genero';
+import Resultados from './screens/Resultados';
 
 function App() {
-
-  const [dataPeliculas, setData] = useState(true);
-
-  useEffect(() => {
-    fetchPopulares('popular');
-  }, []);
-
-  async function fetchPopulares(type) {
-    try {
-      const apiKey = `f14610908c45d33a1bf2ea0c3dcb500d`;
-      let resp = await axios.get(`https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}&language=es-ES&page=1`);
-      console.log(resp.data.results);
-
-      setData(resp.data.results);
-    } catch (error) {
-      console.error(error);
-    }
-
-  }
-
-
   return (
     <Switch>
       <Route path="/" exact = {true} component={Home} />
-      <Route path="/detail" component={Detalle} />
-      <Route path="/fav" component={Favoritos} />
-      <Route path="/result" component={Busqueda} />
-      <Route path="/all" component={VerTodas} />
-      <Route path="/loader" component={Loader} />
+      <Route path="/pelicula/id/:id" component={Detalle} />
+      <Route path="/genero/:genero" component={Generos} />
+      <Route path="/Tusfavoritos" component={Favoritos} />
+      <Route path="/aboutUs" component={AboutUs} />
+      <Route path="/resultados/:resultado" component={Resultados} />
       <Route path="" component={NotFound} />
     </Switch>
   );
